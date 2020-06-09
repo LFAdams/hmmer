38b7114d0ec395e820c44718f4d6eccdc9d117fe
Lincoln Adams <54683715+LFAdams@users.noreply.github.com>
2020-06-09 18:04:10 -0400

    Update megatree.sh

--- megatree.sh
+++ megatree.sh
@@ -1,7 +1,7 @@
 #PBS -S /bin/bash
 #PBS -N megatree
 #PBS -q batch
-#PBS -l nodes=1:ppn=32:AMD
+#PBS -l nodes=1:ppn=8:AMD
 #PBS -l mem=40gb
 #PBS -l walltime=480:00:00
 
@@ -9,4 +9,4 @@ cd /scratch/lfa81121/nbarcid/
 
 module load MEGA/7.0.26-1
 
-megacc -a /home/lfa81121/hmmer/MLLaminotree.mao -d atcucspecnbs_msa.fa -o /scratch/lfa81121/nbarcid/mega/
+megacc -a /home/lfa81121/hmmer/MLLaminotree8.mao -d atcucspecnbs_msa.fa -o /scratch/lfa81121/nbarcid/mega/
